9225eb21b2bb1b03a31983e3499d44246d3e576a
Lucas <suarez.luks@gmail.com>
2019-06-29 16:08:32 +0100

    Starting remove item functionlity

--- js/index.js
+++ js/index.js
@@ -6,13 +6,29 @@ const ulList = document.getElementById('list');
 btn.addEventListener('click', function(){
 	var input = document.getElementById('input').value; // Capture input value
 	var newItem = document.createElement("LI");     // Create a <li> node
-	newItem.innerHTML = input + '<input type="checkbox" class="checkboxes"><p class="delete-x">x</p>';  // Add content to li element for todo.                   
+	newItem.innerHTML = input + '<input type="checkbox" class="checkboxes"><p class="delet" >x</p>';  // Add content to li element for todo.                   
 	ulList.insertBefore(newItem, ulList.childNodes[0]);  // Insert <li> before the first child of <ul>
+	// input = ' ';  // Reset input value to empty field
 })
 
 
 // REMOVE ITEM - FUNCIONALITY _________________________________________________
 
-// Individual deletes
-const deletex = document.getElementsByClassName('delete-x');
-console.log(deletex)
+// Individual deletes - STUCK WITH THIS BELOW
+
+if(document.getElementById('delet') != null){
+	var delet = document.getElementById('delet').innerText;
+	console.log('delet detected: ' + delet); 
+} else { console.log('null'); }
+
+if(delet){
+	console.log(delet + 'epa'); 
+  }
+
+  function deleteTodo() {
+	console.log('qUE pasa'); }
+
+if(delet){
+	delet.addEventListener('click', deleteTodo);
+  }
+
