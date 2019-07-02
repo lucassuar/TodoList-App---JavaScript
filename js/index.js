d4ed85e963d776064424d0e93b61f5fb202ab6e2
Lucas <suarez.luks@gmail.com>
2019-07-02 10:58:42 +0100

    Last functionalitites

--- js/index.js
+++ js/index.js
@@ -1,34 +1,66 @@
-// ADD ITEM - FUNCIONALITY _________________________________________________
+// ADD ITEM, REMOVE ITEM - FUNCIONALITY _________________________________________________
 const btn = document.getElementById('btn');
 const ulList = document.getElementById('list');
 
 // Button event listener with adding li elemnts with input value
 btn.addEventListener('click', function(){
 	var input = document.getElementById('input').value; // Capture input value
-	var newItem = document.createElement("LI");     // Create a <li> node
-	newItem.innerHTML = input + '<input type="checkbox" class="checkboxes"><p class="delet" >x</p>';  // Add content to li element for todo.                   
-	ulList.insertBefore(newItem, ulList.childNodes[0]);  // Insert <li> before the first child of <ul>
+	if (input == "") {
+		alert("Input needed!!");
+	} else {
+		var newItem = document.createElement("LI");     // Create a <li> node
+		newItem.innerHTML = input  // Add content to li element for todo.                 
+		newItem.appendChild(createCheckbox());
+		newItem.appendChild(createDeleteX());
+		ulList.insertBefore(newItem, ulList.childNodes[0]);  // Insert <li> before the first child of <ul>
+	}
+
+
+	
+	
 	// input = ' ';  // Reset input value to empty field
 })
 
 
+// CREATE CHECKBOX AND MARK DONE TODO  - FUNCIONALITY _________________________________________________
+
+function createCheckbox() {
+	const checkbox = document.createElement('input');
+	checkbox.type = 'checkbox';
+	checkbox.className = 'checkboxes';
+	checkbox.addEventListener('change', function () {
+	  if (checkbox.checked) {
+		checkbox.parentNode.classList.add('line-through');
+	  } else {
+		checkbox.parentNode.classList.remove('line-through');
+	  }
+	});
+	
+	return checkbox;
+  }
+
+  // CREATE SINGLE TODO DELETE X  - FUNCIONALITY _________________________________________________
+
+  function createDeleteX() {
+	const deleteX = document.createElement('p');
+	deleteX.className = 'delet';
+	deleteX.innerText = 'x';
+	deleteX.addEventListener('click', function () {
+		this.parentNode.remove(this);
+	});
+	
+	return deleteX;
+  }
+
+
+
 // REMOVE ITEM - FUNCIONALITY _________________________________________________
 
-// Individual deletes - STUCK WITH THIS BELOW
+const clearBtn = document.getElementById('btnClr');
 
-if(document.getElementsByClassName('delet') != null){
-	var delet = document.getElementsByClassName('delet').innerText;
-	console.log('delet detected: ' + delet); 
-} else { console.log('null'); }
-
-if(delet){
-	console.log(delet + 'epa'); 
-  }
-
-  function deleteTodo() {
-	console.log('qUE pasa'); }
-
-if(delet){
-	delet.addEventListener('click', deleteTodo);
-  }
-
+clearBtn.addEventListener('click', function(){
+	var listItem = ulList.getElementsByTagName("li");
+           while(listItem.length > 0){                   
+			ulList.removeChild(listItem[0]);
+           }
+})
